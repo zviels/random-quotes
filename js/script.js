28929182b87b0f257354ac80afb21365df95a880
zviels <59054323+zviels@users.noreply.github.com>
2021-03-24 12:54:57 +0200

    Extended App Functionality

--- js/script.js
+++ js/script.js
@@ -1,350 +1,23 @@
-// The Quotes Array
+ // Variables
 
-let quotes = [
+ let quotes = [];
 
-    {
+ // Getters
 
-        quote: 'We Know What We Are, But Know Not What We May Be.',
-        source: 'William Shakespeare'
+ const getRandomValue = () => Math.floor(Math.random() * 256);
+ const getRandomColor = () => 'rgb(' + getRandomValue() + ', ' + getRandomValue() + ', ' + getRandomValue() + ')';
 
-    },
+ // getRandomQuote
 
-    {
+ const getRandomQuote = () => {
 
-        quote: 'Most People Overestimate What They Can Do In One Year, And Underestimate What They Can Do In Ten Years.',
-        source: 'Bill Gates',
-        tags: ['Motivational', 'Inspirational']
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
 
-    },
+    quotes.splice(randomIndex, 1);
+    return randomQuote;
 
-    {
-
-        quote: 'I Can\'t Change The Direction Of The Wind, But I Can Adjust My Sails To Always Reach My Destination.',
-        source: 'Jimmy Dean'
-
-    },
-
-    {
-
-        quote: 'Always Stay True To Yourself And Never Let What Somebody Says Distract You From Your Goals.',
-        source: 'Michelle Obama'
-
-    },
-
-    {
-
-        quote: 'You Don\'t Always Need A Plan. Sometimes You Just Need To Breathe, Trust, Let Go And See What Happens.',
-        source: 'Mandy Hale'
-
-    },
-
-    {
-                    
-        quote: 'If You Can Dream It - You Can Do It.',
-        source: 'Walt Disney',
-        tags: ['Dreams', 'Motivational', 'Inspirational']
-    
-    },
-
-    {
-        
-        quote: 'Shoot For The Moon. Even If You Miss, You\'ll Still End Up Among The Stars.',
-        source: 'Les Brown',
-        tags: ['Motivational', 'Success', 'Inspirational']
-       
-    },
-
-    {
-        
-        quote: 'You Can Never Quit. Winners Never Quit, And Quitters Never Win.',
-        source: 'Ted Turner',
-        tags: ['Business', 'Inspirational']
-       
-    },
-
-    {
-        
-        quote: 'Always Desire To Learn Something Useful.',
-        source: 'Sophocles',
-        tags: ['Intelligence', 'Knowledge', 'Learning']
-       
-    },
-
-    {
-        
-        quote: 'Veni, Vidi, Vici.',
-        source: 'Julius Caesar',
-        tags: ['History', 'War']
-       
-    },
-
-    {
-        
-        quote: 'A Happy Man Is Too Satisfied With The Present To Dwell Too Much On The Future.',
-        source: 'Albert Einstein',
-        tags: ['Happiness', 'Satisfaction'],
-        citation: '\"Mes Projets d\'Avenir\"',
-        year: 1896
-       
-    },
-
-    {
-        
-        quote: 'When Something Is Important Enough, You Do It Even If The Odds Are Not In Your Favor.',
-        source: 'Elon Musk'
-       
-    },
-
-    {
-        
-        quote: 'Quality Is Not An Act. It Is A Habit.',
-        source: 'Aristotle'
-       
-    },
-
-    {
-        
-        quote: 'The Secret Of Getting Ahead Is Getting Started.',
-        source: 'Mark Twain'
-       
-    },
-
-    {
-        
-        quote: 'Well Done Is Better Than Well Said.',
-        source: 'Benjamin Franklin'
-       
-    },
-
-    {
-        
-        quote: 'The People Who Influence You Are The People Who Believe In You.',
-        source: 'Henry Drummond'
-       
-    },
-
-    {
-        
-        quote: 'Good, Better, Best. Never Let It Rest. \'Til Your Good Is Better And Your Better Is Best.',
-        source: 'St. Jerome'
-       
-    },
-
-    {
-        
-        quote: 'It Does Not Matter How Slowly You Go As Long As You Do Not Stop.',
-        source: 'Confucius'
-       
-    },
-
-    {
-        
-        quote: 'It Always Seems Impossible - Until It\'s Done.',
-        source: 'Nelson Mandela'
-       
-    },
-
-    {
-        
-        quote: 'Act As If What You Do Makes A Difference. It Does.',
-        source: 'William James'
-       
-    },
-
-    {
-        
-        quote: 'Don\'t Watch The Clock; Do What It Does - Keep Going.',
-        source: 'Sam Levenson'
-       
-    },
-
-    {
-        
-        quote: 'Intelligence Is The Ability To Adapt To Changes.',
-        source: 'Stephen Hawking'
-       
-    },
-
-    {
-        
-        quote: 'However Difficult Life May Seem, There Is Always Something You Can Do And Succeed At.',
-        source: 'Stephen Hawking'
-       
-    },
-
-    {
-        
-        quote: 'Success Consists Of Going From Failure To Failure Without Loss Of Enthusiasm.',
-        source: 'Winston Churcill'
-       
-    },
-
-    {
-        
-        quote: 'You Have Enemies? Good. That Means You\'ve Stood Up For Something, Sometime In Your Life.',
-        source: 'Winston Churcill'
-       
-    },
-
-    {
-        
-        quote: 'Do Your Work With Your Whole Heart, And You Will Succeed - There\'s So Little Competition.',
-        source: 'Elbert Hubbard'
-       
-    },
-
-    {
-        
-        quote: 'Learning Never Exhausts The Mind.',
-        source: 'Leonardo Da Vinci'
-       
-    },
-
-    {
-        
-        quote: 'If You Cannot Do Great Things, Do Small Things In A Great Way.',
-        source: 'Napoleon Hill'
-       
-    },
-
-    {
-        
-        quote: 'The Journey Of A Thousand Miles Begins With One Step.',
-        source: 'Lao Tzu'
-       
-    },
-
-    {
-        
-        quote: 'I Have Not Failed. I\'ve Just Found 10,000 Ways That Won\'t Work.',
-        source: 'Thomas Edison'
-       
-    },
-
-    {
-        
-        quote: 'Tell Me And I Forget; Teach Me And I Remember; Involve Me And I Learn.',
-        source: 'Benjamin Franklin'
-       
-    },
-
-    {
-        
-        quote: 'Very Little Is Needed To Make A Happy Life; It Is All Within Yourself, In Your Way Of Thinking.',
-        source: 'Marcus Aurelius'
-       
-    },
-
-    {
-        
-        quote: 'A Leader Is One Who Knows The Way, Goes The Way, And Shows The Way.',
-        source: 'John C. Maxwell'
-       
-    },
-
-    {
-        
-        quote: 'If Opportunity Doesn\'t Knock - Build A Door.',
-        source: 'Milton Berle'
-       
-    },
-
-    {
-        
-        quote: 'Wise Men Speak Because They Have Something To Say; Fools Speak Because They Have To Say Something.',
-        source: 'Plato'
-       
-    },
-
-    {
-        
-        quote: 'The Only True Wisdom Is In Knowing You Know Nothing.',
-        source: 'Socrates'
-       
-    },
-
-    {
-        
-        quote: 'Believe You Can And You\'re Halfway There.',
-        source: 'Theodore Roosevelt'
-       
-    },
-
-    {
-        
-        quote: 'Education Is The Most Powerful Weapon Which You Can Use To Change The World.',
-        source: 'Nelson Mandela'
-       
-    },
-
-    {
-        
-        quote: 'Success Is Not Final, Failure Is Not Fatal - It Is The Courage To Continue That Counts.',
-        source: 'Winston Churcill'
-       
-    },
-
-    {
-        
-        quote: 'Do Not Dwell In The Past, Do Not Dream Of The Future; Concentrate The Mind On The Present Moment.',
-        source: 'Buddha'
-       
-    },
-
-    {
-        
-        quote: 'Nothing Is Impossible. The Word Itself Says \'I\'m Possible\'.',
-        source: 'Audrey Hepburn'
-       
-    },
-
-    {
-        
-        quote: 'A Little More Persistence, A Little More Effort, And What Seemed Hopeless Failure May Turn To Glorious Success.',
-        source: 'Elbert Hubbard'
-       
-    },
-
-    {
-        
-        quote: 'You Are Never Too Old To Set Another Goal Or To Dream A New Dream.',
-        source: 'Les Brown'
-       
-    },
-
-    {
-        
-        quote: 'We Aim Above The Mark To Hit The Mark.',
-        source: 'Ralph Waldo Emerson'
-       
-    },
-
-    {
-        
-        quote: 'I Am A Slow Walker - But I Never Walk Back.',
-        source: 'Abraham Lincoln'
-       
-    }
-
- ];
-
- // Functions
-
- // The 'getRandomQuote' Function
-
- const getRandomQuote = () => quotes [ Math.floor(Math.random() * quotes.length) ];
-
- // The 'getValue' Function Returns A Random Value Between 0 And 255
-
- const getValue = () => Math.floor(Math.random() * 256);
-
- // The 'getColor' Function Returns A Random Color
-
- const getColor = () => 'rgb(' + getValue() + ', ' + getValue() + ', ' + getValue() + ')';
-
- // The 'setColor' Function Changes The Background Color To A Random Color
-
- const setColor = () => document.querySelector('body').style.backgroundColor = getColor();
+ }
 
  // The 'getHTML' Function Returns The HTML Structure Of A Quote
 
@@ -365,7 +38,13 @@ let quotes = [
 
  }
 
- // The 'printTags' Function Displays The Tags Of A Quote, If They Are Exist
+ // Setters
+
+ const setBackgroundColor = () => document.querySelector('body').style.backgroundColor = getRandomColor();
+
+ // Main Functions
+
+ // printTags
 
  const printTags = (randomQuote) => {
 
@@ -384,17 +63,21 @@ let quotes = [
     
  }
 
- // The 'printQuote' Function
+ // printQuote
 
  const printQuote = () => {
 
     const quoteBox = document.querySelector('#quote-box');
+
+    if (quotes.length === 0)
+        quotes = [ ... data ];
+
     const randomQuote = getRandomQuote();
     const html = getHTML(randomQuote);
 
     quoteBox.innerHTML = html;
     printTags(randomQuote);
-    setColor();
+    setBackgroundColor();
     
     clearInterval(interval);
     interval = setInterval(printQuote, 15000);
